bdc18a7a537acc4f0ac070212bd83e2cc124afee
H-unique245 <a.hemant96@gmail.com>
2023-01-05 10:07:55 +0530

    server

--- server.js
+++ server.js
@@ -6,11 +6,27 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+    res.jsonp(req.query)
+  })
+  
+  // To handle POST, PUT and PATCH you need to use a body-parser
+  // You can use the one used by JSON Server
+  server.use(jsonServer.bodyParser)
+  server.use((req, res, next) => {
+      if (req.method === 'POST') {
+          req.body.createdAt = Date.now()
+        }
+        // Continue to JSON Server router
+        next()
+    })
+    server.use(jsonServer.rewriter({
+        '/api/*': '/$1',
+        '/blog/:resource/:id/show': '/:resource/:id'
+    }))
+    server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
