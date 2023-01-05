029da6062ae20c6c1d10f1ac6b67f889176bda2d
H-unique245 <a.hemant96@gmail.com>
2023-01-05 10:59:36 +0530

    server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
+server.use(jsonServer.bodyParser)
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
