101b89d91f3b819bd1066bc1699020b151a57adc
H-unique245 <a.hemant96@gmail.com>
2023-01-10 10:39:26 +0530

    server auth added

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+server.db = router.db
+// server.bind(router)
+
+// const rules = auth.rewriter({
+//   // Permission rules
+//   users: 600,
+//   messages: 640,
+//   // Other rules
+//   '/posts/:category': '/posts?category=:category',
+// })
+
+// You must apply the middlewares in the following order
+
 
 server.use(middlewares)
+server.use(auth)
+// server.use(rules)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
@@ -13,8 +28,12 @@ server.use(jsonServer.rewriter({
 server.use(jsonServer.bodyParser)
 server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running')
+  console.log('JSON Server is running')
 })
+
 
 // Export the Server API
 module.exports = server
+
+// json-server-auth backend
+
